ab9e4036193911c23cab67dfeb9a4b7d4ac792b0
sumit298 <sumitsinha1007@gmail.com>
2025-11-10 14:43:05 +0530

    added frontend with monolithic

---
+++ frontend/app/page.tsx
@@ -0,0 +1,19 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+      <div className="">
+
+        <h1 className="text-5xl md:text-7xl font-bold bg-gradient-to-r from-accent-purple to-accent-pink bg-clip-text text-transparent mb-4">
+          StreamHub
+        </h1>
+
+        <p className="text-xl md:text-2xl text-text-secondary max-w-2xl mx-auto">
+          Your platform for live streaming. Connect with audiences, share
+          your passion, and build your community.
+        </p>
+
+      </div>
+    </div>
+  );
+}
