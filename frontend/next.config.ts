67129580f51dc11c76cb91f0cd87576292aacbc1
sumit298 <sumitsinha1007@gmail.com>
2026-06-17 00:36:22 +0530

    image path fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    optimizePackageImports: ['lucide-react'],
+    optimizePackageImports: ["lucide-react"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+        
+      },
+    ],
+    domains: ["api.dicebear.com"],
   },
 };
 
